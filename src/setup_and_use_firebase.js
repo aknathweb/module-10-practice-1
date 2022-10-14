7a4bb0331dbe7622ee897badbc8b4b0b79ad6f9b
aknathweb <aniknwu.cse@gmail.com>
2022-10-14 19:47:38 +0600

    firebase using working procedure step by stem

---
+++ src/setup_and_use_firebase.js
@@ -0,0 +1,36 @@
+/* 
+/////////////////////////////////////////////
+Create Firebase Project + setup vs Code
+////////////////////////////////////////////
+go firebase: https://firebase.google.com/ 
+> Go to console >> Add project 
+>>> give project name and create a project
+>>>>Add Firebase to your web app >>>>> Give app name
+>>>>>> Add Firebase SDK (follow npm for vs code setup) and
+(initialise Firebase and begin using the SDKs for the products that you'd like to use.
+(copy and paste firebase.init.js))
+
+/////////////////////////////////////////////
+Authentication from 3rd party Application
+////////////////////////////////////////////
+> Go to console >> select project >>> Build 
+>>>> Authentication >>>>> Get started 
+>>>>>> Sign-in method Authentication
+>>>>>>> Google >>>>>>>> just email add
+>>>>>>> Github 
+{   
+    >Go Github Account >> Settings >>> Developer settings
+    >>>>GitHub Apps >>>>> New GitHub App
+    >>>>>> Register new GitHub App
+    >>>>>>> Callback URL (from Firebase)
+}
+>>>>>>>> Collect AppID and Client ID
+
+/////////////////////////////////////////////
+Set 3rd party app authentication
+////////////////////////////////////////////
+> Go to docs >> Build >>> Authentication 
+>>>> Web 
+>>>>> Get started >>>>> Follow Code steps
+>>>>> Google/GitHUb >>>>> Follow Code steps
+*/
